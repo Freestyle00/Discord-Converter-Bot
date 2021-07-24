d18065376463ce703c12787ce33a691b26e243fc
Freestyle00 <nick@rudal.eu>
2021-07-24 02:38:06 +0200

    Finished the class for converting

--- Converter.py
+++ Converter.py
@@ -24,27 +24,58 @@ class Converting:
         return Celsius
     def MeasurmentWorldtoUS(self, number, mmcmmkm):
         """
-        In here our mesurment gets converted into their
-        measurment
+        In here our measurment gets converted into US
+        standards
         """
-        unit = ""
         mmcmmkm = mmcmmkm.lower()
         if mmcmmkm == "mm": #inch
-            unit == "mm"
-            return unit
+            # mm × 0.039370078740157 = inch 
+            inch = number * 0.039370078740157
+            inch = round(inch, 3)
+            return f"{inch}in"
         elif mmcmmkm == "cm": #foot
-            unit = "cm"
-            return unit
+            # cm * 0.032808 = foot
+            foot = number * 0.032808
+            foot = round(foot, 3)
+            return f"{foot}ft"
         elif mmcmmkm == "m": #yard
-            unit = "m"
-            return unit
+            # m * 1.093613
+            yard = number * 1.093613
+            yard = round(yard, 3)
+            return f"{yard}yd"
         elif mmcmmkm == "km": #mile
-            unit = "km"   
-            return unit
+            # km * 0.6213712
+            mile = number * 0.6213712
+            mile = round(mile, 3)
+            return f"{mile}mi"
         else:
             #raise Exception("Something has gone wrong please check if statements")
-            return "Something has gone wrong while figuring out thenit"
-
-name = Converting()
-
-print(name.MeasurmentUStoWorld(1, "Cm"))
+            return "Something has gone wrong while figuring out the unit"
+    def MeasurmentUStoWorld(self, number, inftydmi):
+        """
+        This to convert the US measurment into
+        something more understandable for us
+        """
+        inftydmi = inftydmi.lower()
+        if inftydmi == "in":
+            # inch * 25.4
+            milimeter = number * 25.4
+            milimeter = round(milimeter, 3)
+            return f"{milimeter}mm"
+        elif inftydmi == "ft":
+            # feet * 30.48
+            centimeter = number * 30.48
+            centimeter = round(centimeter, 3)
+            return f"{centimeter}cm"
+        elif inftydmi == "yd":
+            # yard * 0.9144
+            meter = number * 0.9144
+            meter = round(meter, 3)
+            return f"{meter}m"
+        elif inftydmi == "mi":
+            # mile * 1.609344
+            kilometer = number * 1.609344
+            kilometer = round(kilometer, 3)
+            return f"{kilometer}km"
+        else:
+            return "Something has gone wrong while figuring out the unit"
